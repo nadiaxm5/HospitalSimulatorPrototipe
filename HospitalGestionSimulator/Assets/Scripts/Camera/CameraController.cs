e25782cf5f3021902d3dbdb29f8db50f66e75fa7
Namomi5 <90263390+nadiaxm5@users.noreply.github.com>
2024-02-20 10:25:50 +0100

    Update Input System

--- HospitalGestionSimulator/Assets/Scripts/Camera/CameraController.cs
+++ HospitalGestionSimulator/Assets/Scripts/Camera/CameraController.cs
@@ -3,15 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     Vector3 moveVector = Vector3.zero;
+    Vector2 moveInput = Vector2.zero;
     [SerializeField] float camMoveSpeed = 2.5f;
     [SerializeField] float camRotateSpeed = 1f;
     Vector3 rotateVector;
+    float rotationAngle;
     [SerializeField] CinemachineVirtualCamera virtualCamera;
     float targetFOV;
+    float zoomInput;
     [SerializeField] float minFOV = 35f;
     [SerializeField] float maxFOV = 90f;
     [SerializeField] float zoomSensitivity = 5f;
@@ -24,40 +28,42 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CameraMove();
-        CameraRotate();
-        CameraZoom();
+        MoveCamera();
+        RotateCamera();
+        ZoomCamera();
     }
 
-    private void CameraZoom()
+    public void OnInputMoveCamera(InputAction.CallbackContext context)
     {
-        targetFOV += -(Input.mouseScrollDelta.y * zoomSensitivity); //Zoom by mouse scroll
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnInputRotateCamera(InputAction.CallbackContext context)
+    {
+        rotationAngle = context.ReadValue<float>();
+    }
+
+    public void OnInputZoomCamera(InputAction.CallbackContext context)
+    {
+        zoomInput = context.ReadValue<float>();
+    }
+
+    private void ZoomCamera()
+    {
+        targetFOV += zoomInput * zoomSensitivity; //Zoom by mouse scroll
         targetFOV = Mathf.Clamp(targetFOV, minFOV, maxFOV); //Limit zoom
         virtualCamera.m_Lens.FieldOfView = targetFOV;
     }
 
-    private void CameraRotate() //Rotation with Q and E
+    private void RotateCamera() //Rotation with Q and E
     {
-        rotateVector = Vector3.zero;
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotateVector.y = camRotateSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            rotateVector.y = -(camRotateSpeed * Time.deltaTime);
-        }
-
+        rotateVector.y = -rotationAngle * camRotateSpeed * Time.deltaTime;
         transform.eulerAngles += rotateVector;
     }
 
-    private void CameraMove() //Movement with WASD
+    private void MoveCamera() //Movement with WASD
     {
-        moveVector.x = Input.GetAxisRaw("Horizontal");
-        moveVector.z = Input.GetAxisRaw("Vertical");
-
-        moveVector = transform.right * moveVector.x + transform.forward * moveVector.z;
-
+        moveVector = transform.right * moveInput.x + transform.forward * moveInput.y;
         transform.position += moveVector * Time.deltaTime * camMoveSpeed;
     }
 }

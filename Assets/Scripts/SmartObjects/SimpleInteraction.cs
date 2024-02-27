e9f2463c533fd6823940ae45692fcc4b0bcb09c7
Namomi5 <90263390+nadiaxm5@users.noreply.github.com>
2024-02-27 13:45:37 +0100

    Seguir con los smart objects

--- Assets/Scripts/SmartObjects/SimpleInteraction.cs
+++ Assets/Scripts/SmartObjects/SimpleInteraction.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class SimpleInteraction : MonoBehaviour
+public class SimpleInteraction : BaseInteraction
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected int MaxSimultaneousUsers = 1;
+
+    protected int NumCurrentUsers = 0;
+
+    public override bool CanPerform()
     {
-        
+        return NumCurrentUsers < MaxSimultaneousUsers;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void LockInteraction()
     {
-        
+        ++NumCurrentUsers;
+        if(NumCurrentUsers > MaxSimultaneousUsers)
+        {
+            Debug.LogError($"Too many users have locked this interaction: {_DisplayName}");
+        }
+    }
+
+    public override void Perform(MonoBehaviour performer, UnityEvent<BaseInteraction> onCompleted = null)
+    {
+        if(NumCurrentUsers <= 0)
+        {
+            Debug.LogError($"Trying to perform an interaction when there are no users: {_DisplayName}");
+            return;
+        }
+
+        //Comprobar tipo de interacción
+        if(InteractionType == EInteractionType.Instantaneous)
+        {
+            if(onCompleted != null)
+                onCompleted.Invoke(this);
+        }
+
+        else if(InteractionType == EInteractionType.OverTime)
+        {
+            //Video en 25:00
+        }
+    }
+
+    public override void UnLockInteraction()
+    {
+        if(NumCurrentUsers <= 0)
+        {
+            Debug.LogError($"Trying to unlock an already unlocked interaction: {_DisplayName}");
+        }
+        --NumCurrentUsers;
     }
 }

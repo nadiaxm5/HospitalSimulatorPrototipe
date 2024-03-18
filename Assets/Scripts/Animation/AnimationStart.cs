fb3233bdb5a0a9687803890859319f6f3322c1c8
izankuni <al404504@uji.es>
2024-03-18 10:18:40 +0100

    Animación de la puerta

---
+++ Assets/Scripts/Animation/AnimationStart.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStart : MonoBehaviour
+{
+    Animator animator;
+    GameObject player;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100) && hit.transform.name == transform.name)
+            {
+                float distance = Vector3.Distance(player.transform.position, transform.position);
+                if (distance <= 2f)
+                    animator.enabled = true;
+            }
+        }
+    }
+}

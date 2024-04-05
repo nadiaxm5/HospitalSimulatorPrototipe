ba8ddee002d573e6a0a871dad04d9f8a9c183b73
izankuni <al404504@uji.es>
2024-04-05 10:33:06 +0200

    Video añadadido y bug fixes

--- Assets/Scripts/Dialogues/DialogueManager.cs
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
-using Ink.UnityIntegration;
 using UnityEngine.EventSystems;
 
 
@@ -17,7 +16,7 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private GameObject[] choices;
 
     [Header("Globals Ink File")]
-    [SerializeField] private InkFile globalsInkFile;
+    [SerializeField] private TextAsset loadGlobalsJSON;
     [SerializeField] private AudioClip textSound;
 
     private static DialogueManager instance;
@@ -34,7 +33,7 @@ public class DialogueManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
-        dialogueVariables = new DialogueVariables(globalsInkFile.filePath);
+        dialogueVariables = new DialogueVariables(loadGlobalsJSON);
         typingSpeed = 0.01f;
     }
 
--- Assets/Scripts/Dialogues/DialogueVariables.cs
+++ Assets/Scripts/Dialogues/DialogueVariables.cs
@@ -9,19 +9,18 @@ public class DialogueVariables
 {
     public Dictionary<string, Ink.Runtime.Object> variables { get; private set; }
 
-    public DialogueVariables(string globalsFilePath)
+    public DialogueVariables(TextAsset loadGlobalsJSON)
     {
-        //Compilar la historia
-        string inkFileContents = File.ReadAllText(globalsFilePath);
-        Ink.Compiler compiler = new Ink.Compiler(inkFileContents);
-        Story globalVariablesStory = compiler.Compile();
+        // create the story
+        Story globalVariablesStory = new Story(loadGlobalsJSON.text);
 
-        //Inicializar el diccionario
+        // initialize the dictionary
         variables = new Dictionary<string, Ink.Runtime.Object>();
         foreach (string name in globalVariablesStory.variablesState)
         {
             Ink.Runtime.Object value = globalVariablesStory.variablesState.GetVariableWithName(name);
             variables.Add(name, value);
+            Debug.Log("Initialized global dialogue variable: " + name + " = " + value);
         }
     }
 
